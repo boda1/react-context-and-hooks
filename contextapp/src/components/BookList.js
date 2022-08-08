f42f66db8924de9ea6f3b9b1daa0f0cd917e104c
Adam Boda <adam.boda@MBP-ADAMBODA.local>
2022-08-08 18:36:00 +0100

    added git ignore file and consumed bookcontext

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,18 +1,22 @@
 import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 const BookList = () => {
     const {isLightTheme, light, dark} = useContext(ThemeContext);
     const theme = isLightTheme ? light : dark;
+    const bookList = useContext(BookContext);
+    // <li key={book.id} style={{ background: theme.ui }}>{book.title}</li>)
+    console.log(bookList);
     return (
         <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
             <ul>
-                <li style={{ background: theme.ui }}>Wild swans</li>
-                <li style={{ background: theme.ui }}>Dune</li>
-                <li style={{ background: theme.ui }}>Ham on Rye</li>
+                {bookList.books.map(book => <li key={book.id} style={{ background: theme.ui }}>{book.title}</li>)}
             </ul>
         </div>
     );
 }
  
 export default BookList;
+
+
